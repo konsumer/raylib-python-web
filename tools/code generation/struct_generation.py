2e6239385e99b43071026fe23387bdbb1e778e1a
dor283 <dor.shapira@outlook.com>
2023-07-18 02:08:17 +0300

    implemented simple __init__ code generation of structs

--- tools/code generation/struct_generation.py
+++ tools/code generation/struct_generation.py
@@ -22,7 +22,7 @@ def struct_member_heap_kind(member: CType) -> HeapKind:
         case CTypeKind.Void:
             raise SyntaxError("void type shouldn't be checked for size")
         case CTypeKind.Pointer:
-            return HeapKind.UInt8
+            return HeapKind.UInt32
         case CTypeKind.I8:
             return HeapKind.Int8
         case CTypeKind.UI8:
@@ -52,9 +52,9 @@ def struct_member_heap_kind(member: CType) -> HeapKind:
 def struct_member_to_python_type_hint(member: CType):
     match member.kind:
         case CTypeKind.Void:
-            raise SyntaxError("void type shouldn't be checked")
+            return ""
         case CTypeKind.Pointer:
-            return ""  # TODO: we can do better
+            return f"\"{struct_member_to_python_type_hint(member.of)}*\""
         case CTypeKind.I8 | CTypeKind.UI8 | \
              CTypeKind.I16 | CTypeKind.UI16 | \
              CTypeKind.I32 | CTypeKind.UI32 | \
@@ -81,6 +81,9 @@ def generate_struct_code(struct_api) -> str:
     # add init method
     string += f"    def __init__("
     for member_ctype, member_json in zip(struct_.members, struct_api['fields']):
+        if member_ctype.kind == CTypeKind.Array:
+            return ""  # TODO: implement struct code generation for structs that has members of array type
+
         string += f"{member_json['name']}"
         type_hint: str = struct_member_to_python_type_hint(member_ctype)
         if type_hint != "":
@@ -92,7 +95,45 @@ def generate_struct_code(struct_api) -> str:
     string: str = string[0:-2]
     string += f"):\n"
 
+    # malloc code of class
+    string += f"        self._address = _mod._malloc({struct_.size})\n"
+
+    # set self values
+    offset: int = 0
+    for member_ctype, member_json in zip(struct_.members, struct_api['fields']):
+        if member_ctype.kind == CTypeKind.Array:
+            return ""  # TODO: implement struct code generation for structs that has members of array type
+
+        if member_ctype.kind != CTypeKind.Struct:
+            string += f"        self._{member_json['name']} = {member_json['name']}\n"
+            heap_kind: CTypeKind = struct_member_heap_kind(member_ctype)
+            match heap_kind:
+                case CTypeKind.Void:
+                    raise SyntaxError("void type shouldn't be checked for size")
+                case HeapKind.Int8:
+                    string += f"        _mod.HEAP8[self._address + {offset}] = {member_json['name']}\n"
+                case HeapKind.UInt8:
+                    string += f"        _mod.HEAPU8[self._address + {offset}] = {member_json['name']}\n"
+                case HeapKind.Int16:
+                    string += f"        _mod.HEAP16[self._address + {offset}] = {member_json['name']}\n"
+                case HeapKind.UInt16:
+                    string += f"        _mod.HEAPU16[self._address + {offset}] = {member_json['name']}\n"
+                case HeapKind.Int32:
+                    string += f"        _mod.HEAP32[self._address + {offset}] = {member_json['name']}\n"
+                case HeapKind.UInt32:
+                    string += f"        _mod.HEAPU32[self._address + {offset}] = {member_json['name']}\n"
+                case HeapKind.Float32:
+                    string += f"        _mod.HEAPF32[self._address + {offset}] = {member_json['name']}\n"
+                case HeapKind.Float64:
+                    string += f"        _mod.HEAPF32[self._address + {offset}] = {member_json['name']}\n"
+        else:
+            string += f"        self._{member_json['name']} = {member_json['name']}\n"
+
+
+        offset += get_ctype_size(member_ctype)
+
     return string
+
 
 for struct_api in raylib_api_structs:
     struct_string = generate_struct_code(struct_api)
