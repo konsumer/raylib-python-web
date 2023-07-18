0e63dcc5aa214eb9c4c6a52b02f59db84839b3ec
David Konsumer <konsumer@jetboystudio.com>
2023-07-17 17:09:06 -0700

    start of work on FOnt

--- demo/wrapper.py
+++ demo/wrapper.py
@@ -1,8 +1,27 @@
+# Generic array-like collection that uses wasm as memory-back
+class StructArray:
+    def __init__(self, stype, length, address=None):
+        self._stype = stype
+        self._length = length
+        self._size = self._stype._size * self._length
+        if address is not None:
+            self._address:int = address
+        else:
+            self._address: int = _mod._malloc(self._size)
+    def __del__(self):
+        _mod._free(self._address)
+    
+    def __len__(self):
+        return self._length
+
+    def __getitem__(self, item):
+        return self._stype(address=(self._stype._size * item))
+
 class Color:
     _size: int = 4
     def __init__(self, r: int = 0, g: int = 0, b: int = 0, a: int = 0, frozen=False, address=None):
         self._frozen = frozen
-        if address:
+        if address is not None:
             self._address:int = address
         else:
             self._address: int = _mod._malloc(self._size)
@@ -50,29 +69,11 @@ class Color:
         if not self._frozen:
             _mod.HEAPU8[self._address + 3] = value
 
-
-class StructArray:
-    def __init__(self, stype, length, frozen=False, address=None):
-        self._stype = stype
-        self._length = length
-        self._frozen = frozen
-        self._size = self._stype._size * self._length
-        if address:
-            self._address:int = address
-        else:
-            self._address: int = _mod._malloc(self._size)
-    def __del__(self):
-        _mod._free(self._address)
-
-    def __getitem__(self, item):
-        return self._stype(address=(self._stype._sizee * item))
-
-
 class Rectangle:
     _size: int = 16
     def __init__(self, x: float = 0, y: float = 0, width: float = 0, height: float = 0, frozen=False, address=None):
         self._frozen = frozen
-        if address:
+        if address is not None:
             self._address:int = address
         else:
             self._address: int = _mod._malloc(self._size)
@@ -115,11 +116,156 @@ class Rectangle:
     def height(self):
         return _mod.HEAPF32[self._address + 12]
 
-    @width.setter
+    @height.setter
     def height(self, value):
         if not self._frozen:
             _mod.HEAPF32[self._address + 12] = value
 
+class Texture:
+    _size:int = 20
+    def __init__(self, tid: int = 0, width: int = 0, height: int = 0, mipmaps: int = 0, tformat: int = 0, frozen=False, address=None):
+        self._frozen = frozen
+        if address is not None:
+            self._address:int = address
+        else:
+            self._address: int = _mod._malloc(self._size)
+            _mod.HEAP32[self._address + 0] = tid
+            _mod.HEAP32[self._address + 4] = width
+            _mod.HEAP32[self._address + 8] = height
+            _mod.HEAP32[self._address + 12] = mipmaps
+            _mod.HEAP32[self._address + 16] = tformat
+
+    @property
+    def tid(self):
+        return _mod.HEAPU32[self._address + 0]
+
+    @tid.setter
+    def tid(self, value):
+        if not self._frozen:
+            _mod.HEAPU32[self._address + 0] = value
+
+    @property
+    def width(self):
+        return _mod.HEAP32[self._address + 4]
+
+    @width.setter
+    def width(self, value):
+        if not self._frozen:
+            _mod.HEAP32[self._address + 4] = value
+
+    @property
+    def height(self):
+        return _mod.HEAP32[self._address + 8]
+
+    @height.setter
+    def height(self, value):
+        if not self._frozen:
+            _mod.HEAP32[self._address + 8] = value
+
+    @property
+    def mipmaps(self):
+        return _mod.HEAP32[self._address + 12]
+
+    @mipmaps.setter
+    def mipmaps(self, value):
+        if not self._frozen:
+            _mod.HEAP32[self._address + 12] = value
+
+    @property
+    def tformat(self):
+        return _mod.HEAP32[self._address + 16]
+
+    @tformat.setter
+    def tformat(self, value):
+        if not self._frozen:
+            _mod.HEAP32[self._address + 16] = value
+
+class Font:
+    _size:int = 40
+    def __init__(self, baseSize: int = 0, glyphCount: int = 0, glyphPadding: int = 0, texture=None, recs=None, glyphs=None, frozen=False, address=None):
+        self._frozen = frozen
+        if address is not None:
+            self._address:int = address
+        else:
+            self._address: int = _mod._malloc(self._size)
+            _mod.HEAP32[self._address + 0] = baseSize
+            _mod.HEAP32[self._address + 4] = glyphCount
+            _mod.HEAP32[self._address + 8] = glyphPadding
+            if texture is not None:
+                struct_clone(texture, self._address + 12)
+            if recs is not None:
+                _mod.HEAP32[self._address + 32] = recs._address
+            if glyphs is not None:
+                _mod.HEAP32[self._address + 36] = glyphs._address
+
+    def __del__(self):
+        _mod._free(self._address)
+
+    @property
+    def baseSize(self):
+        return _mod.HEAP32[self._address + 0]
+
+    @baseSize.setter
+    def baseSize(self, value):
+        if not self._frozen:
+            _mod.HEAP32[self._address + 0] = value
+
+    @property
+    def glyphCount(self):
+        return _mod.HEAP32[self._address + 4]
+
+    @glyphCount.setter
+    def glyphCount(self, value):
+        if not self._frozen:
+            _mod.HEAP32[self._address + 4] = value
+
+    @property
+    def glyphPadding(self):
+        return _mod.HEAP32[self._address + 8]
+
+    @glyphPadding.setter
+    def glyphPadding(self, value):
+        if not self._frozen:
+            _mod.HEAP32[self._address + 8] = value
+
+    @property
+    def texture(self):
+        return Texture(address=self._address + 12)
+
+    @texture.setter
+    def texture(self, value):
+        if not self._frozen:
+            struct_clone(value, self._address + 12)
+
+    @property
+    def recs(self):
+        # TODO: where does recs.length come from?
+        length=10
+        return StructArray(Rectangle, length, address=self._address + 32)
+
+    @recs.setter
+    def recs(self, value):
+        if not self._frozen:
+            struct_clone(value, self._address + 32)
+
+    @property
+    def glyphs(self):
+        glyphCount = _mod.HEAP32[self._address + 4]
+        return StructArray(GlyphInfo, glyphCount, address=self._address + 36)
+
+    @glyphs.setter
+    def glyphs(self, value):
+        if not self._frozen:
+            struct_clone(value, self._address + 36)
+
+
+# TODO: these need to be filled in for Font to work right
+
+class Image:
+    _size:int = 20
+
+class GlyphInfo:
+    _size:int = 52
 
 
 LIGHTGRAY = Color(200, 200, 200, 255, frozen=True)  # Light Gray
@@ -152,14 +298,20 @@ RAYWHITE = Color(245, 245, 245, 255, frozen=True)  # My own White (raylib logo)
 
 # helper to copy a struct
 # newColor = sturct_clone(RAYWHITE)
+# newColor._frozen = false
+# newColor.a = 127
 # TODO: _memcpy is not exposed for some reason
-def struct_clone(source):
-    a = _mod._malloc(source._size)
+def struct_clone(source, a):
+    if not a:
+        a = _mod._malloc(source._size)
     _mod._memcpy(a, source._address, source._size)
     out = source.__class__(address = a)
-    out._frozen = False
     return out
 
+def GetFontDefault():
+    ret = Font()
+    _mod._GetFontDefault(ret._address)
+    return ret
 
 def ClearBackground(color):
     _mod._ClearBackground(color._address)
