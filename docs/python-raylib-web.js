f75c69efa1127e9f17da53653567dd78d2492fea
David Konsumer <konsumer@jetboystudio.com>
2023-09-16 02:08:49 -0700

    move demo to docs for pages

--- docs/python-raylib-web.js
+++ docs/python-raylib-web.js
@@ -1,5 +1,7 @@
 import Module from './raylib.js'
 
+const loc = import.meta.url.replace(/python-raylib-web\.js$/, '')
+
 export default async function setup (canvas) {
   const mod = await Module({ canvas })
   mod.mem = new DataView(mod.HEAPU8.buffer)
@@ -7,8 +9,10 @@ export default async function setup (canvas) {
   window.mod = mod
   pyodide.globals.set('_mod', mod)
 
+  console.log()
+
   // just for dev, pull wrapper from seperate file
-  const wrapper = await fetch('wasmraypy/__init__.py').then(r => r.text())
+  const wrapper = await fetch(`${loc}/wasmraypy/__init__.py`).then(r => r.text())
   pyodide.runPython(wrapper)
 
   return pyodide
