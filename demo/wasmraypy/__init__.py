0494d9a1026f17c1ad014c38d4c8ab13b7199496
דור שפירא <Dor.Shapira@outlook.com>
2023-09-16 10:01:12 +0300

    upadte __init__.py

--- demo/wasmraypy/__init__.py
+++ demo/wasmraypy/__init__.py
@@ -5530,31 +5530,3 @@ BLANK: Color = Color(0, 0, 0, 0, frozen=True)  # Blank (Transparent)
 MAGENTA: Color = Color(255, 0, 255, 255, frozen=True)  # Magenta
 RAYWHITE: Color = Color(245, 245, 245, 255, frozen=True)  # My own White (raylib logo)
 
-
-LIGHTGRAY = Color(200, 200, 200, 255, frozen=True)  # Light Gray
-GRAY = Color(130, 130, 130, 255, frozen=True)  # Gray
-DARKGRAY = Color(80, 80, 80, 255, frozen=True)  # Dark Gray
-YELLOW = Color(253, 249, 0, 255, frozen=True)  # Yellow
-GOLD = Color(255, 203, 0, 255, frozen=True)  # Gold
-ORANGE = Color(255, 161, 0, 255, frozen=True)  # Orange
-PINK = Color(255, 109, 194, 255, frozen=True)  # Pink
-RED = Color(230, 41, 55, 255, frozen=True)  # Red
-MAROON = Color(190, 33, 55, 255, frozen=True)  # Maroon
-GREEN = Color(0, 228, 48, 255, frozen=True)  # Green
-LIME = Color(0, 158, 47, 255, frozen=True)  # Lime
-DARKGREEN = Color(0, 117, 44, 255, frozen=True)  # Dark Green
-SKYBLUE = Color(102, 191, 255, 255, frozen=True)  # Sky Blue
-BLUE = Color(0, 121, 241, 255, frozen=True)  # Blue
-DARKBLUE = Color(0, 82, 172, 255, frozen=True)  # Dark Blue
-PURPLE = Color(200, 122, 255, 255, frozen=True)  # Purple
-VIOLET = Color(135, 60, 190, 255, frozen=True)  # Violet
-DARKPURPLE = Color(112, 31, 126, 255, frozen=True)  # Dark Purple
-BEIGE = Color(211, 176, 131, 255, frozen=True)  # Beige
-BROWN = Color(127, 106, 79, 255, frozen=True)  # Brown
-DARKBROWN = Color(76, 63, 47, 255, frozen=True)  # Dark Brown
-WHITE = Color(255, 255, 255, 255, frozen=True)  # White
-BLACK = Color(0, 0, 0, 255, frozen=True)  # Black
-BLANK = Color(0, 0, 0, 0, frozen=True)  # Blank (Transparent)
-MAGENTA = Color(255, 0, 255, 255, frozen=True)  # Magenta
-RAYWHITE = Color(245, 245, 245, 255, frozen=True)  # My own White (raylib logo)
-    
